5ba932e40eb306453c994de837b4f1965d1417dd
Rabia <sdetcode@gmail.com>
2022-02-27 23:09:40 +0530

    renamed application properties

--- src/main/java/com/example/microservices/limitsservice/Configuration.java
+++ src/main/java/com/example/microservices/limitsservice/Configuration.java
@@ -1,7 +1,9 @@
 package com.example.microservices.limitsservice;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties("limits-service")
 public class Configuration {
     private int minimum;
